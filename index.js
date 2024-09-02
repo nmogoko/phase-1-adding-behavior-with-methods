4584cc90f41d5e6958be0c9b5bc5640fc91c81a4
nmogoko <+>
2024-09-02 10:53:33 +0100

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    return `${this.name} says meow!`;
+  }
+}
+const cat = new Cat("Sasha", "female");
+cat.speak();
+
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    return `${this.name} says woof!`;
+  }
+}
+const dog = new Dog("Rex", "male");
+console.log(dog.speak());
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    if (this.sex === "male") {
+      return `It's me! ${this.name}, the parrot!`;
+    } else {
+      return `${this.name} says squawk!`;
+    }
+  }
+}
+const bird = new Bird("Pablo", "male");
+bird.speak();
